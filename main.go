783454ad5a63f6c7b431d155f1eb5cddeac375a7
Ricky Whitaker <ricky.whitaker@grafana.com>
2023-06-01 17:07:27 -0500

    code update

--- main.go
+++ main.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("Initial commit")
 	fmt.Println("commit from develop")
 	fmt.Println("commit 2 from develop")
+	fmt.Println("Let's cause a security patch merge conflict!")
 }
