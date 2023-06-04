0f51b500f7454551f2a314864f2dc732693e38cb
Ricky Whitaker <ricky.whitaker@grafana.com>
2023-06-03 23:22:57 -0500

    security patch 2

--- main.go
+++ main.go
@@ -8,4 +8,5 @@ func main() {
 	fmt.Println("commit 2 from develop")
 	fmt.Println("Let's cause a security patch merge conflict!")
 	fmt.Println("Security patch 1")
+	fmt.Println("Security patch 2")
 }
